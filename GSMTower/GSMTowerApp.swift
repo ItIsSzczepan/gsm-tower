815785684d92adf4c857dc47a8d826c025fc40be
jakub.szczepaniak <jakub.szczepaniak@appchance.com>
2025-07-01 11:54:48 +0200

    Initial Commit

---
+++ GSMTower/GSMTowerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GSMTowerApp.swift
+//  GSMTower
+//
+//  Created by Jakub Szczepaniak on 01/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct GSMTowerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GSMTowerTests/GSMTowerTests.swift
@@ -0,0 +1,17 @@
+//
+//  GSMTowerTests.swift
+//  GSMTowerTests
+//
+//  Created by Jakub Szczepaniak on 01/07/2025.
+//
+
+import Testing
+@testable import GSMTower
+
+struct GSMTowerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
